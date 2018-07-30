15460ba4988c50451bbdae8b3185ee0d879fface
kathyleboeuf <kleboeuf@townisp.com>
2018-07-30 16:04:28 -0400

    added 2nd get req

--- server.js
+++ server.js
@@ -13,11 +13,18 @@ const app = express();
 app.use(express.static('public'));
 
 app.listen(8080, function() {
-	console.info(`Server listening on ${this.address().port}`);
+  console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
-	console.error(err);
+  console.error(err);
 });
 
 app.get('/api/notes', (req, res) => {
-	res.json(data);
+  res.json(data);
 });
+
+app.get('/api/notes/:id', (req, res) => {
+	const id  = req.params.id;
+	const findObjByMatchId = data.find(item => item.id === Number(id));
+  res.json(findObjByMatchId);
+});
+
