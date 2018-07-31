10ac0be734a800ba80e42de8705b969aead66ece
kathyleboeuf <kleboeuf@townisp.com>
2018-07-31 16:11:08 -0400

    tuesday error with router

---
+++ router/notes.router.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+
+//const router = express.Router();
+//const data = require('./db/notes');
+
+router.get('/api/notes', (req, res) => {
+  const searchTerm = req.query.searchTerm;
+  if(searchTerm) {
+    //console.log(searchBy); // test for searchTerm in server console
+    const find = data.filter(note => note.title.includes(searchTerm) || note.content.includes(searchTerm));
+    //console.log(find);
+    res.json(find);
+  } else {
+    res.json(data);
+  }
+
+});
+
+router.get('/api/notes/:id', (req, res) => {
+  const id  = req.params.id;
+  const findObjByMatchId = data.find(item => item.id === Number(id));
+  res.json(findObjByMatchId);
+});
+
+
+
+module.exports = {}
+
+
+
+
