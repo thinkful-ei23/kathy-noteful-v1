028242e31134d9e5024bcf1e513717ad647f1d11
kathyleboeuf <kleboeuf@townisp.com>
2018-07-31 17:38:25 -0400

    added middleware error-handling

--- server.js
+++ server.js
@@ -38,12 +38,25 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(findObjByMatchId);
 });
 
+//temporary code to check runtime error generation
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+//temporary code to check runtime error generation
+
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   res.status(404).json({ message: 'Not Found' });
 });
 
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
 
 //const { PORT }  = require('./config');
 
