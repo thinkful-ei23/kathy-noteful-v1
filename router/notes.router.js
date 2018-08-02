3d51db0ae33ae67bc6ab8032eeb994021578d669
kathyleboeuf <kleboeuf@townisp.com>
2018-08-01 21:40:42 -0400

    added POST method

--- router/notes.router.js
+++ router/notes.router.js
@@ -1,33 +1,111 @@
 'use strict';
 
 const express = require('express');
+const router = express.Router();
 
-//const router = express.Router();
-//const data = require('./db/notes');
+const data = require('../db/notes');
+const simDB = require('../db/simDB');  // <<== add this
+const notes = simDB.initialize(data);
 
-// router.get('/api/notes', (req, res) => {
-//   const searchTerm = req.query.searchTerm;
-//   if(searchTerm) {
-//     //console.log(searchBy); // test for searchTerm in server console
-//     const find = data.filter(note => note.title.includes(searchTerm) || note.content.includes(searchTerm));
-//     //console.log(find);
-//     res.json(find);
-//   } else {
-//     res.json(data);
-//   }
+//GET all and search by query
+router.get('/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
 
-// });
+  notes.filter(searchTerm, (err, list)  => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(list); //responds with filtered array
 
+  });
+});
+//GET single with params
+router.get('/notes/:id', (req, res, next) => {
+  const { id } = req.params;
 
-// router.get('/api/notes/:id', (req, res) => {
-//   const id  = req.params.id;
-//   const findObjByMatchId = data.find(item => item.id === Number(id));
-//   res.json(findObjByMatchId);
-// });
+  notes.find(id, (err, item) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(item); //responds with filtered array
+  });
 
+  // const id  = req.params.id;
+  // const findObjByMatchId = data.find(item => item.id === Number(id));
+  // res.json(findObjByMatchId);
+});
+// PUT update an item
+router.put('/notes/:id', (req, res, next) => {
+  const id = req.params.id;
 
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
 
-module.exports = {}
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  /***** Never trust users - validate input *****/
+  if (!updateObj.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+// POST (insert) an item
+router.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+// Delete an item
+router.delete('/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  notes.delete(id, (err) => {
+    if (err) {
+      return next(err);
+    }
+    res.sendStatus(204);
+  });
+});
+
+//+++++++++++++++++++++++
+
+module.exports = router;
 
 
 
--- server.js
+++ server.js
@@ -1,87 +1,38 @@
 'use strict';
-// create EXPRESS app
-const express = require('express');
-const requestLogger = require('./middleware/logger');
 
+const express = require('express');
+const morgan = require('morgan');
+
+
+const { PORT } = require('./config');
+const notesRouter = require('./router/notes.router');
+
+
+// create EXPRESS app
 const app = express();
+
 //logs all requests
-app.use(requestLogger);
+app.use(morgan('dev'));
+
 //static file server
 app.use(express.static('public'));
+
 //parse request body
 app.use(express.json());
 
-// Load array of notes
-const data = require('./db/notes');
-const simDB = require('./db/simDB');  // <<== add this
-const notes = simDB.initialize(data); // <<== and this
+// mount the router
+app.use('/api', notesRouter);
 
 
-//console.log('Hello Noteful!');
-
-
-//++++++++++++++++++++++++++++++++++++++++++
-
-app.get('/api/notes', (req, res, next) => {
-  const { searchTerm } = req.query;
-
-  notes.filter(searchTerm, (err, list)  => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); //responds with filtered array
-
-  });
-});
-
-app.get('/api/notes/:id', (req, res, next) => {
-  const { id } = req.params;
-
-  notes.find(id, (err, item) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(item); //responds with filtered array
-  });
-
-  // const id  = req.params.id;
-  // const findObjByMatchId = data.find(item => item.id === Number(id));
-  // res.json(findObjByMatchId);
-});
-
-app.put('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
-
-  /***** Never trust users - validate input *****/
-  const updateObj = {};
-  const updateFields = ['title', 'content'];
-
-  updateFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
-
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
-
-//++++++++++++++++++++++++++++++++++++++++++
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
-  res.status(404).json({ message: 'Not Found' });
+  next(err);
+  //res.status(404).json({ message: 'Not Found' });
 });
 
-app.use(function (err, req, res, next) {
+// Catch-all error handler
+app.use(function (err, req, res/*, next*/) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
@@ -90,18 +41,8 @@ app.use(function (err, req, res, next) {
 });
 //++++++++++++++++++++++++++++++++++++++++++
 
-app.listen(8080, function() {
+app.listen(PORT, function() {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
 });
-
-
-// if(searchTerm) {
-//   //console.log(searchBy); // test for searchTerm in server console
-//   const find = data.filter(note => note.title.includes(searchTerm) || note.content.includes(searchTerm));
-//   //console.log(find);
-//   res.json(find);
-// } else {
-//   res.json(data);
-// }
