1ca4774a3b882d51bfe9b97888f2202120b41bbf
kathyleboeuf <kleboeuf@townisp.com>
2018-08-05 19:57:28 -0400

    test static server-done with app replacing server line 54

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
+'use strict';
+
 module.exports.PORT = 8080;
--- server.js
+++ server.js
@@ -40,9 +40,13 @@ app.use(function (err, req, res/*, next*/) {
   });
 });
 //++++++++++++++++++++++++++++++++++++++++++
+//listen for incoming connections
+if (require.main === module) {
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+}
 
-app.listen(PORT, function() {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
+module.exports = app; //export for testing
